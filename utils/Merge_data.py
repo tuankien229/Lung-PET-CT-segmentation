0be546f8400b7ade76247c7cd4610183349ad4cc
tuankien229 <tuankien229@gmail.com>
2023-03-25 16:09:51 +0700

    update merge data

--- utils/Merge_data.py
+++ utils/Merge_data.py
@@ -6,6 +6,11 @@ Created on Mon Mar 20 15:55:24 2023
 """
 import glob
 import pandas as pd
+import shutil
+
+source_dir = r"E:\demos\files\reports"
+destination_dir = r"E:\demos\files\account"
+
 
 def merge_data(meta_path, annotaion_path):
     dict_folder={'folder_location':[], 'folder_name':[], 'xml_location':[], 'xml_name':[]}
@@ -17,11 +22,17 @@ def merge_data(meta_path, annotaion_path):
                 xml_location = glob.glob(annotaion_path + '/' + xml_name)
                 if len(xml_location) > 0 :
                     xml_location = xml_location[0]
-                    dict_folder['folder_location'].append(folder_location)
-                    dict_folder['folder_name'].append(folder_name)
-                    dict_folder['xml_location'].append(xml_location)
-                    dict_folder['xml_name'].append(xml_name)
-                    break
+                    for folder_pet in  glob.glob(folder_location + '/*'):
+                        if len(glob.glob(folder_pet + '/*')) > 3:
+                            try:
+                                shutil.copytree(folder_pet, f'dataset/{folder_name}/pet_ct/')
+                                shutil.copytree(xml_location, f'dataset/{folder_name}/annotation/')
+                            except:
+                                pass
+                            dict_folder['folder_location'].append(f'dataset/pet_ct/{folder_name}')
+                            dict_folder['folder_name'].append(folder_name)
+                            dict_folder['xml_location'].append(f'dataset/annotation/{xml_name}')
+                            dict_folder['xml_name'].append(xml_name)
     df = pd.DataFrame.from_dict(dict_folder)
     df.to_csv('PET-CT-merged.csv')
 
